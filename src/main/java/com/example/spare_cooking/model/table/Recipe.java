8759ef55e882be79168f691b4b533f943fd6781a
Bored moon <899804@mail.ru>
2026-03-30 15:59:33 +0500

    Few changes

--- src/main/java/com/example/spare_cooking/model/table/Recipe.java
+++ src/main/java/com/example/spare_cooking/model/table/Recipe.java
@@ -1,4 +1,9 @@
 package com.example.spare_cooking.model.table;
 
+import org.springframework.data.annotation.Id;
+
 public class Recipe {
+
+
+    private Long id;
 }
---
+++ src/main/java/com/example/spare_cooking/model/table/User.java
@@ -0,0 +1,41 @@
+package com.example.spare_cooking.model.table;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHashed;
+    private String email;
+    private String passwordHash;
+    private String avatarUrl;
+    private String bio;
+    private LocalDateTime createdAt;
+    //@OneToMany(mappedBy = "author")
+    //private List<Recipe> recipes;
+//
+//    @ManyToMany
+//    @JoinTable(name = "saved_recipes",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "recipe_id"))
+//    private Set<String> savedRecipes; // сохранённые рецепты
+
+    //@OneToMany(mappedBy = "user")
+    //private List<String> comments;
+
+    //@OneToMany(mappedBy = "user")
+    //private List<String> likes;
+}
