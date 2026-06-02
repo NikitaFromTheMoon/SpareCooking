401e4207c94312092865805d25492ca28118563d
Bored moon <899804@mail.ru>
2026-06-02 17:07:27 +0500

    Recipe repo fix

--- src/main/java/com/example/spare_cooking/repository/RecipeRepository.java
+++ src/main/java/com/example/spare_cooking/repository/RecipeRepository.java
@@ -2,13 +2,19 @@ package com.example.spare_cooking.repository;
 
 import com.example.spare_cooking.model.table.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
 
     Page<Recipe> findByTagsName(String name, Pageable pageable);
+    Page<Recipe> findByTagsName(List<String> name, Pageable pageable);
+    Optional<Recipe> findById(Long id);
     Page<Recipe> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
